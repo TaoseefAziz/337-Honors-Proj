ad380fee4910751cfba541476af9886a3a9ded11
TaoseefAziz <taoseefaziz@arizona.edu>
2023-05-05 14:05:42 -0700

    Refactored game html using context and loop

--- wordtris/game/views.py
+++ wordtris/game/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+context = {
+    'n': range(200),
+    'm': range(10),
+    'k': range(16),
+    # other context stuff
+}
+
 # Create your views here.
 def game(request):
-    return render(request,'game/game.html',{})
+    return render(request,'game/game.html',context)
