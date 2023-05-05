360f24e83ede4d0997bea99d7d747f445a6ae9d9
TaoseefAziz <taoseefaziz@arizona.edu>
2023-05-05 13:50:31 -0700

    Many to one database created

--- wordtris/game/admin.py
+++ wordtris/game/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import Game
 from .models import Player
 
-
 # Register your models here.
+
+admin.site.register(Game)
+admin.site.register(Player)
--- wordtris/game/models.py
+++ wordtris/game/models.py
@@ -2,21 +2,22 @@ from django.db import models
 import datetime
 # Create your models here.
 
+class Player(models.Model):
+    username = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"Username: {self.username}" 
+
 class Game(models.Model):
     score = models.IntegerField()
     words_matched = models.IntegerField()
     longest_word = models.IntegerField()
     most_valuable_word = models.CharField(max_length=20)
-    date_played = models.DateField(_("Date"), auto_now_add=True)
-
-def __str__(self):
-    return f"Score: {self.score} Words Matched: {self.words_matched} Date Played: {self.date_played}" 
-
-
-class Player(models.Model):
-    username = models.CharField(max_length=20)
-    games_played = models.ForeignKey(Game, blank='True',null='True')
+    date_played = models.DateField(default=datetime.date.today, blank=True)
+    player = models.ForeignKey(Player, blank='True',null='True',on_delete=models.CASCADE)
 
 
     def __str__(self):
-        return f"Username: {self.username} Games Played: {self.games_played}" 
+        return f"Score: {self.score} Words Matched: {self.words_matched} Date Played: {self.date_played}" 
+
+
