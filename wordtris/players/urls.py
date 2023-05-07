3b763579e5ce4093249d1b847aeb4994eb82ea51
TaoseefAziz <taoseefaziz@arizona.edu>
2023-05-06 19:09:41 -0700

    registration created

--- wordtris/players/urls.py
+++ wordtris/players/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('login_player',views.login_player, name="login"),
+    path('register_player',views.register_player, name="register_player"),
+    path('logout_player',views.logout_player, name="logout"),
 ]
